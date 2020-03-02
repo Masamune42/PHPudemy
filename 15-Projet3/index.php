521d304fa6566d88eb7f8a517e8174de8fcf435e
alex <a.caignard@hotmail.fr>
2020-03-02 22:16:08 +0100

    début projet 3 : Raccourcisseur URL

---
+++ 15-Projet3/index.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="design/default.css">
+    <link rel="icon" href="pictures/favico.png"/>
+    <title>Raccourcisseur d'URL Express</title>
+</head>
+<body>
+<!-- PRESENTATION -->
+<section id="hello">
+    <!-- CONTAINER -->
+    <div class="container">
+        <header>
+            <img src="pictures/logo.png" alt="header" id="logo">
+        </header>
+        <!-- VP -->
+        <h1>Une URL longue? Raccourcissez-là</h1>
+        <h2>Largement meilleur et plus court que les autres</h2>
+
+        <!-- FORM -->
+        <form method="post" action="">
+            <input type="url" name="url" placeholder="Collez un lien à raccourcir">
+            <input type="submit" value="Raccourcir">
+        </form>
+    </div>
+</section>
+
+<!-- BRANDS -->
+<section id="brands">
+    <!-- CONTAINER -->
+    <div class="container">
+        <h3>Ces marques nous font confiance</h3>
+    </div>
+    <img src="pictures/1.png" alt="1" class="picture">
+    <img src="pictures/2.png" alt="2" class="picture">
+    <img src="pictures/3.png" alt="3" class="picture">
+    <img src="pictures/4.png" alt="4" class="picture">
+</section>
+
+<!-- FOOTER -->
+<footer>
+    <img src="pictures/logo2.png" alt="logo" id="logo"><br>
+    2020 &copy; Bitly<br>
+    <a href="">Contact</a> -
+    <a href="">A propos</a>
+</footer>
+
+</body>
+</html>
+
+<?php
